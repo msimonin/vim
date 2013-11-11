56f96338454a2688e53d59cfc3e381cca2bf3e7d
msimonin <matthieu.simonin@inria.fr>
2013-11-11 10:14:17 +0100

    update install M

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
 curl -Sso ~/.vim/autoload/pathogen.vim \
 https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+cd ~/vim
+git submodule init
+git submodule update
+
