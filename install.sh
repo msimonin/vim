3cd5b6d6b1218d123d745d6743bd71851651e847
msimonin <matthieu.simonin@inria.fr>
2013-11-11 10:23:24 +0100

    fix path M

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-mkdir -p ~/.vim/autoload ~/.vim/bundle; \
-curl -Sso ~/.vim/autoload/pathogen.vim \
+mkdir -p ~/vim/autoload ~/vim/bundle; \
+curl -Sso ~/vim/autoload/pathogen.vim \
 https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 cd ~/vim
